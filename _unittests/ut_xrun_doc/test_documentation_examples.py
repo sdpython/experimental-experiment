398ba4cf0c9be63d757b2a0f24550106fbd93720
Xavier Dupre <xadupre@microsoft.com>
2023-12-05 14:33:01 +0100

    fix unit tests

--- _unittests/ut_xrun_doc/test_documentation_examples.py
+++ _unittests/ut_xrun_doc/test_documentation_examples.py
@@ -67,9 +67,9 @@ class TestDocumentationExamples(ExtTestCase):
         fold = os.path.normpath(os.path.join(this, "..", "..", "_doc", "examples"))
         found = os.listdir(fold)
         for name in found:
-            if name in {"plot_onnx_export.py"}:
-                if sys.platform in {"darwin", "win32"}:
-                    # Stuck due to the creation of a secondary process.
+            if name in {"plot_torch_export.py"}:
+                if sys.platform in {"win32"}:
+                    # dynamo not supported on windows
                     continue
 
             if name.startswith("plot_") and name.endswith(".py"):
