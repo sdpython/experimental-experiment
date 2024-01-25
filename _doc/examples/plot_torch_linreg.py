fcf192b27f2cf651a9b8085ae6c35a558ed5c302
Xavier Dupre <xadupre@microsoft.com>
2024-01-25 16:54:07 +0100

    ruff

--- _doc/examples/plot_torch_linreg.py
+++ _doc/examples/plot_torch_linreg.py
@@ -9,7 +9,6 @@ data
 ====
 """
 
-import numpy as np
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
