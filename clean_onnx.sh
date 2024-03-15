6ea7925fa3e2932fade86d85d1193077f8c51280
Xavier Dupre <xadupre@microsoft.com>
2024-03-15 13:44:29 +0100

    doc

--- clean_onnx.sh
+++ clean_onnx.sh
@@ -9,11 +9,13 @@ rm temp* -rf
 rm dump* -rf
 rm *.sarif
 rm *.svg
+rm dump_models -rf
 rm neural_coder_workspace -rf
 
 rm _doc/examples/plot*.onnx
 rm _doc/examples/plot*.txt
 rm _doc/examples/ort*.onnx
+rm _doc/examples/*.sarif
 rm _doc/examples/*.json
 rm _doc/examples/*.png
 rm _doc/examples/*.csv
@@ -22,3 +24,4 @@ rm _doc/examples/dummy*.onnx
 rm _doc/examples/*.opt.onnx
 rm _doc/examples/*.dynamo.onnx
 rm _doc/examples/*.script.onnx
+rm _doc/examples/dump_models -rf
