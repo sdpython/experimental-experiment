e362e6ca7d5fd4bfe4655f8650755ae0a37fe85b
Xavier Dupre <xadupre@microsoft.com>
2024-03-28 12:59:53 +0100

    fix domain

--- experimental_experiment/reference/ops/op_scatternd_of_shape.py
+++ experimental_experiment/reference/ops/op_scatternd_of_shape.py
@@ -4,7 +4,7 @@ from onnx.reference.ops.op_scatternd import _scatter_nd_impl
 
 
 class ScatterNDOfShape(OpRun):
-    op_domain = "com.microsoft"
+    op_domain = "onnx_extended.ortops.optim.cuda"
 
     def _run(self, shape, indices, updates, reduction=None):
         data = np.zeros(shape, dtype=updates.dtype)
