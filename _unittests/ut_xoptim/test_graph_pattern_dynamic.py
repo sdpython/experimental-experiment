e50b97529854f1c94fe58fbdae24eda93935f9ea
Xavier Dupre <xadupre@microsoft.com>
2024-05-10 10:15:22 +0200

    fix a few issues

--- _unittests/ut_xoptim/test_graph_pattern_dynamic.py
+++ _unittests/ut_xoptim/test_graph_pattern_dynamic.py
@@ -27,17 +27,76 @@ class TestGraphPatternDynamic(ExtTestCase):
         self.assertExists(p)
         return load_onnx(p)
 
+    def test_check_one(self):
+        static_model = self._get_model("shape-dort-static-llama-custom__0.onnx")
+        dynamic_model = self._get_model("shape-dort-dynamic-llama-custom__0.onnx")
+        opts = OptimizationOptions(
+            patterns="ReshapeMatMulReshape",
+            verbose=10 if __name__ == "__main__" else 0,
+        )
+        gr1 = GraphBuilder(
+            static_model, infer_shapes=True, optimization_options=opts, verbose=0
+        )
+        gr1.optimize()
+        if __name__ == "__main__":
+            print("----------------------------------------")
+            print("----------------------------------------")
+            print("----------------------------------------")
+        gr1 = GraphBuilder(
+            dynamic_model, infer_shapes=True, optimization_options=opts, verbose=0
+        )
+        gr1.optimize()
+
     def test_graph_default_forward_single(self):
         static_model = self._get_model("shape-dort-static-llama-custom__0.onnx")
         dynamic_model = self._get_model("shape-dort-dynamic-llama-custom__0.onnx")
         patterns = get_default_patterns()
-        self._check_models_patterns(static_model, dynamic_model, patterns, False)
+        self._check_models_patterns(
+            static_model,
+            dynamic_model,
+            patterns,
+            False,
+            exceptions=["ExpandPattern", "ReshapeMatMulReshapePattern"],
+        )
+
+    def test_graph_default_backward_single(self):
+        static_model = self._get_model("shape-dort-static-llama-custom__1.onnx")
+        dynamic_model = self._get_model("shape-dort-dynamic-llama-custom__1.onnx")
+        patterns = get_default_patterns()
+        self._check_models_patterns(
+            static_model,
+            dynamic_model,
+            patterns,
+            False,
+            exceptions=[],
+        )
 
     def test_graph_default_forward_cumulative(self):
         static_model = self._get_model("shape-dort-static-llama-custom__0.onnx")
         dynamic_model = self._get_model("shape-dort-dynamic-llama-custom__0.onnx")
         patterns = get_default_patterns()
-        self._check_models_patterns(static_model, dynamic_model, patterns, True)
+        self._check_models_patterns(
+            static_model,
+            dynamic_model,
+            patterns,
+            True,
+            exceptions=[
+                "ExpandPattern",
+                "ReshapeMatMulReshapePattern",
+            ],
+        )
+
+    def test_graph_default_backward_cumulative(self):
+        static_model = self._get_model("shape-dort-static-llama-custom__1.onnx")
+        dynamic_model = self._get_model("shape-dort-dynamic-llama-custom__1.onnx")
+        patterns = get_default_patterns()
+        self._check_models_patterns(
+            static_model,
+            dynamic_model,
+            patterns,
+            True,
+            exceptions=["SwitchOrderBinaryPattern"],
+        )
 
     def _check_models_patterns(
         self,
@@ -45,6 +104,7 @@ class TestGraphPatternDynamic(ExtTestCase):
         model2: ModelProto,
         patterns: List[PatternOptimization],
         cumulative: bool,
+        exceptions: List[str],
     ):
         self.assertNotEqual(len(model1.graph.node), len(model2.graph.node))
         delta = 0
@@ -57,18 +117,23 @@ class TestGraphPatternDynamic(ExtTestCase):
             stat1 = gr1.optimize()
             gr2 = GraphBuilder(model2, infer_shapes=True, optimization_options=opts)
             stat2 = gr2.optimize()
-            app1 = [s for s in stat1 if s["pattern"].startswith("apply")]
-            app2 = [s for s in stat2 if s["pattern"].startswith("apply")]
-            if len(app1) > len(app2) - delta:
+            pat = patterns[i]
+            prefix = f"apply_{pat.__class__.__name__}"
+            app1 = [s for s in stat1 if s["pattern"].startswith(prefix)]
+            app2 = [s for s in stat2 if s["pattern"].startswith(prefix)]
+            if pat.__class__.__name__ in exceptions:
+                assert (
+                    len(app1) > 0
+                ), f"Issue with pattern {patterns[i]} and app1={app1}"
+                continue
+            if len(app1) > len(app2):
                 raise AssertionError(
                     f"Discrancies static/dynamic: i={i}, delta={delta}, "
                     f"len(app1)={len(app1)}, len(app2)={len(app2)}, "
                     f"pattern={patterns[i]}\n-----\n"
                     f"#applied static={len(app1)}, #applied dynamic={len(app2)}"
-                    f"\n{app1}\n--------\n{app2}"
+                    f"\n---------\n{app1}\n--------\n{app2}"
                 )
-            assert len(app1) <= len(app2)
-            delta = len(app2) - len(app1) if cumulative else 0
 
 
 if __name__ == "__main__":
