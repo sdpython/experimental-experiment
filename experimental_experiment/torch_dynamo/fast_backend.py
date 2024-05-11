747dc32440755a8c59fceb2112e432742f3898af
Xavier Dupre <xadupre@microsoft.com>
2024-05-11 10:25:56 +0200

    remove print

--- experimental_experiment/torch_dynamo/fast_backend.py
+++ experimental_experiment/torch_dynamo/fast_backend.py
@@ -524,7 +524,6 @@ def onnx_custom_backend(
     onnx models, graph module as well the inputs and outputs when
     the model is run.
     """
-    print("--- CUSTOM", id(graph_module), id(graph_module.graph))
     assert dump_patterns is None or isinstance(
         dump_patterns, str
     ), f"Unexpected type {type(dump_patterns)} for dump_patterns."
