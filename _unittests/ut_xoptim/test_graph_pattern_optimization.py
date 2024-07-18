5b75292df5cfc4b33637967d84314502088f89e4
Xavier Dupre <xadupre@microsoft.com>
2024-07-18 17:44:01 +0200

    fix issues

--- _unittests/ut_xoptim/test_graph_pattern_optimization.py
+++ _unittests/ut_xoptim/test_graph_pattern_optimization.py
@@ -150,7 +150,7 @@ class TestGraphPatternOptimization(ExtTestCase):
         gr = GraphBuilder(
             origin,
             optimization_options=OptimizationOptions(
-                patterns=["UnsqueezeUnsqueeze"], verbose=0
+                patterns=["UnsqueezeUnsqueeze"], verbose=10
             ),
         )
         res, out, err = self.capture(lambda: gr.optimize_with_patterns())
@@ -168,7 +168,7 @@ class TestGraphPatternOptimization(ExtTestCase):
         before = [node for node in origin.graph.node if node.op_type == "Cast"]
         gr = GraphBuilder(
             origin,
-            optimization_options=OptimizationOptions(patterns=["Cast"], verbose=0),
+            optimization_options=OptimizationOptions(patterns=["Cast"], verbose=10),
         )
         res, out, err = self.capture(lambda: gr.optimize_with_patterns())
         self.assertEmpty(err)
@@ -188,7 +188,7 @@ class TestGraphPatternOptimization(ExtTestCase):
         gr = GraphBuilder(
             origin,
             optimization_options=OptimizationOptions(
-                patterns=["ReshapeMatMulReshape"], verbose=0
+                patterns=["ReshapeMatMulReshape"], verbose=10
             ),
             infer_shapes=True,
         )
