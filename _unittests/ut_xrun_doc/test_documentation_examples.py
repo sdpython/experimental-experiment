787c95e703878a46e7ae244a46cb81d5fd0b8999
xadupre <xadupre@microsoft.com>
2024-09-20 10:52:10 +0200

    disable tests

--- _unittests/ut_xrun_doc/test_documentation_examples.py
+++ _unittests/ut_xrun_doc/test_documentation_examples.py
@@ -6,7 +6,12 @@ import importlib.util
 import subprocess
 import time
 from experimental_experiment import __file__ as experimental_experiment_file
-from experimental_experiment.ext_test_case import ExtTestCase, is_windows, is_apple
+from experimental_experiment.ext_test_case import (
+    ExtTestCase,
+    is_windows,
+    is_apple,
+    has_onnxruntime_training,
+)
 
 VERBOSE = 0
 ROOT = os.path.realpath(
@@ -129,6 +134,18 @@ class TestDocumentationExamples(ExtTestCase):
             if name in {"plot_llama_bench_102.py"}:
                 if sys.platform in {"darwin"}:
                     reason = "apple not supported"
+
+            if name in {
+                "plot_torch_custom_backend_101.py",
+                "lot_llama_bench_102.py",
+                "plot_custom_backend_llama_102.py",
+            }:
+                if not has_onnxruntime_training(True):
+                    reason = "OrtValueVector.push_back_batch is missing (onnxruntime)"
+
+            if name in {"plot_convolutation_matmul_102.py"}:
+                if not has_onnxruntime_training(True):
+                    reason = "OrtModuleGraphBuilder is missing (onnxruntime)"
 
             if not reason and name in {
                 # "plot_convolutation_matmul.py",
