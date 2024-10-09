04ed5c404ad899862cc7cef40ad8caf20c80d8e2
xadupre <xadupre@microsoft.com>
2024-10-09 19:13:38 +0200

    fix issues

--- _unittests/ut_issues/test_issue_pytorch_2024.py
+++ _unittests/ut_issues/test_issue_pytorch_2024.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from onnx.checker import check_model
-from experimental_experiment.ext_test_case import ExtTestCase
+from experimental_experiment.ext_test_case import ExtTestCase, requires_cuda, hide_stdout
 from experimental_experiment.torch_interpreter import to_onnx
 
 
@@ -147,7 +147,7 @@ class TestIssuesOnnxExporter(ExtTestCase):
                     "value_states",
                 ],
                 output_names=["attn_output"],
-                opset=13,
+                opset_version=13,
             )
         elif exporter == "dynamo":
             torch.onnx.export(
@@ -227,7 +227,7 @@ class TestIssuesOnnxExporter(ExtTestCase):
                 onnx_file_path,
                 input_names=["input"],
                 output_names=["output"],
-                opset=18,
+                opset_version=18,
             )
         elif exporter == "dynamo":
             torch.onnx.export(
@@ -301,7 +301,7 @@ class TestIssuesOnnxExporter(ExtTestCase):
                 onnx_file_path,
                 input_names=["input0"],
                 output_names=["qkv_out"],
-                opset=11,
+                opset_version=11,
             )
         elif exporter == "dynamo":
             torch.onnx.export(
@@ -329,14 +329,162 @@ class TestIssuesOnnxExporter(ExtTestCase):
         self.assertEqual(expected_output.shape, output[0].shape)
         self.assertEqualArray(expected_output, output[0], atol=1e-4)
 
+    @requires_cuda()
     def test__flash_attn_script(self):
         self._flash_attn("script")
 
+    @requires_cuda()
     def test__flash_attn_dynamo(self):
         self._flash_attn("dynamo")
 
+    @requires_cuda()
     def test__flash_attn_custom(self):
         self._flash_attn("custom")
+
+    def _complex_weights(self, exporter):
+        # https://github.com/onnx/onnx/issues/6388
+
+        import torch
+
+        class CLinear(torch.nn.Module):
+            def __init__(self, in_features, out_features, bias=True):
+                super().__init__()
+                self.weight = torch.nn.Parameter(
+                    torch.zeros(out_features, in_features, dtype=torch.complex64)
+                )
+                self.bias = torch.nn.Parameter(
+                    torch.zeros(1, out_features, dtype=torch.complex64), requires_grad=bias
+                )
+
+                torch.nn.init.xavier_uniform_(self.weight)
+                if bias:
+                    torch.nn.init.xavier_uniform_(self.bias)
+
+            def forward(self, inp):
+                return torch.matmul(inp, self.weight.T) + self.bias
+
+        example_input = torch.ones((2, 10)).to(torch.complex64)
+        model = CLinear(10, 5).eval()
+        model(example_input)
+        onnx_file_path = f"test_complex_weights_{exporter}.onnx"
+
+        # with torch.no_grad():
+        if exporter == "script":
+            torch.onnx.export(
+                model,
+                (example_input,),
+                onnx_file_path,
+                input_names=["input0"],
+                output_names=["out"],
+                opset_version=18,
+            )
+        elif exporter == "dynamo":
+            torch.onnx.export(
+                model,
+                (example_input,),
+                onnx_file_path,
+                input_names=["input0"],
+                output_names=["out"],
+                dynamo=True,
+            )
+        else:
+            to_onnx(model, (example_input,), filename=onnx_file_path)
+
+        import onnxruntime
+
+        sess_options = onnxruntime.SessionOptions()
+        session = onnxruntime.InferenceSession(
+            onnx_file_path,
+            sess_options=sess_options,
+            providers=[("CPUExecutionProvider")],
+        )
+        inputs_names = [i.name for i in session.get_inputs()]
+        output = session.run(None, dict(zip(inputs_names, (example_input.numpy(),))))
+        expected_output = model(example_input)
+        self.assertEqual(expected_output.shape, output[0].shape)
+        self.assertEqualArray(expected_output, output[0], atol=1e-4)
+
+    def test_complex_weights_script(self):
+        self._complex_weights("script")
+
+    @hide_stdout()
+    def test_complex_weights_dynamo(self):
+        self._complex_weights("dynamo")
+
+    def test_complex_weights_custom(self):
+        self._complex_weights("custom")
+
+    def _slice_4d(self, exporter):
+        # https://github.com/onnx/onnx/issues/6420
+
+        import torch
+
+        class DummySlice(torch.nn.Module):
+            def forward(self, x):
+                x1 = x[:, :, 0:1, :]
+                x2 = x[:, :, 1:2, :]
+                return x1 + x2
+
+        example_input = torch.ones((3, 4, 2, 5)).to(torch.float16)
+        model = DummySlice().eval()
+        model(example_input)
+        onnx_file_path = f"test_slice_4d_{exporter}.onnx"
+
+        # with torch.no_grad():
+        if exporter == "script":
+            torch.onnx.export(
+                model,
+                (example_input,),
+                onnx_file_path,
+                input_names=["input0"],
+                output_names=["output"],
+                opset_version=13,
+                dynamic_axes={"input0": {0: "batch_size"}},
+                do_constant_folding=True,
+                export_params=True,
+            )
+        elif exporter == "dynamo":
+            torch.onnx.export(
+                model,
+                (example_input,),
+                onnx_file_path,
+                input_names=["input"],
+                output_names=["output"],
+                dynamo=True,
+                dynamic_axes={"input": {0: "batch_size"}},
+                fallback=False,
+            )
+        else:
+            batch = torch.export.Dim("batch", min=1, max=1024)
+            to_onnx(
+                model,
+                (example_input,),
+                filename=onnx_file_path,
+                dynamic_shapes={"x": {0: batch}},
+            )
+
+        import onnxruntime
+
+        sess_options = onnxruntime.SessionOptions()
+        session = onnxruntime.InferenceSession(
+            onnx_file_path,
+            sess_options=sess_options,
+            providers=[("CPUExecutionProvider")],
+        )
+        inputs_names = [i.name for i in session.get_inputs()]
+        output = session.run(None, dict(zip(inputs_names, (example_input.numpy(),))))
+        expected_output = model(example_input)
+        self.assertEqual(expected_output.shape, output[0].shape)
+        self.assertEqualArray(expected_output, output[0], atol=1e-4)
+
+    def test_dyn_slice_4d_script(self):
+        self._slice_4d("script")
+
+    def test_dyn_slice_4d_dynamo(self):
+        self._slice_4d("dynamo")
+
+    def test_dyn_slice_4d_custom(self):
+        self._slice_4d("custom")
 
 
 if __name__ == "__main__":
