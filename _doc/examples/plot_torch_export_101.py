8ee921f43d56a74b81ec164344636c07b8d22efd
xadupre <xadupre@microsoft.com>
2024-10-26 00:49:20 +0200

    examples

--- _doc/examples/plot_torch_export_101.py
+++ _doc/examples/plot_torch_export_101.py
@@ -1,11 +1,16 @@
 """
 .. _l-plot-torch-export-101:
 
+=================================================
 101: Some dummy examples with torch.export.export
 =================================================
 
+:func:`torch.export.export` behaviour in various situations.
+
 Easy Case
-+++++++++
+=========
+
+A simple model.
 """
 
 import torch
@@ -227,3 +232,108 @@ swapped_gm = torch.export._swap._swap_modules(exported_program, {"my_neuron": Ne
 
 print("-- preserved?")
 print(swapped_gm.graph)
+
+
+####################################################
+# A bigger example: Phi
+# =====================
+
+import pprint
+import torch
+import torch.export._swap
+from transformers.models.phi3.modeling_phi3 import (
+    Phi3Attention,
+    Phi3LongRoPEScaledRotaryEmbedding,
+    Phi3RMSNorm,
+)
+from experimental_experiment.torch_test_helper import string_type
+from experimental_experiment.torch_models.llm_model_helper import get_phi_35_mini_instruct
+from experimental_experiment.torch_interpreter.onnx_export_errors import (
+    bypass_export_some_errors,
+)
+from experimental_experiment.bench_run import max_diff
+
+
+phi, inputs = get_phi_35_mini_instruct(num_hidden_layers=1, _attn_implementation="eager")
+
+print("model type", type(phi))
+print("inputs:", string_type(inputs))
+
+expected = phi(**inputs)
+
+###################################
+# The module it contains
+# ++++++++++++++++++++++
+
+types = set(type(inst) for _, inst in phi.named_modules())
+print("interesting types:")
+pprint.pprint(list(types))
+
+###################################
+# The interesting module
+# ++++++++++++++++++++++
+
+
+attentions = {
+    name: inst
+    for name, inst in phi.named_modules()
+    if isinstance(inst, (Phi3RMSNorm, Phi3Attention, Phi3LongRoPEScaledRotaryEmbedding))
+}
+
+print("names found:", tuple(sorted(attentions)))
+
+###################################
+# The exported program.
+
+
+with bypass_export_some_errors():
+    exported_program = torch.export.export(
+        phi,
+        tuple(),
+        inputs,
+        strict=False,
+        preserve_module_call_signature=tuple(attentions),
+    )
+print(exported_program.graph)
+
+###################################
+# Checking they are the same.
+
+new_outputs = exported_program.module()(**inputs)
+
+print("--", string_type(expected), string_type(new_outputs))
+
+for k in expected:
+    print(
+        f"-- max_diff for {k}: types "
+        f"{string_type(expected[k])}, {string_type(new_outputs[k])}: "
+        f"max_diff={max_diff(expected[k], new_outputs[k])}"
+    )
+
+############################
+# With attention preserved
+
+swapped_gm = torch.export._swap._swap_modules(exported_program, attentions)
+
+print("--- the new graph")
+print(swapped_gm.graph)
+
+
+###################################
+# Checking again they are the same.
+# Some limitations.
+
+try:
+    new_outputs = swapped_gm(**inputs)
+except ValueError as e:
+    print(f"ERROR: {e}")
+    new_outputs = None
+
+if new_outputs is not None:
+    print("--", string_type(expected), string_type(new_outputs))
+    for k in expected:
+        print(
+            f"-- max_diff for {k}: types "
+            f"{string_type(expected[k])}, {string_type(new_outputs[k])}: "
+            f"max_diff={max_diff(expected[k], new_outputs[k])}"
+        )
---
+++ _doc/examples/plot_torch_export_compile_102.py
@@ -0,0 +1,90 @@
+"""
+.. _l-plot-torch-export-compile-101:
+
+======================
+102: Tweak onnx export
+======================
+
+export or compile
+=================
+"""
+
+import pprint
+import torch
+from experimental_experiment.torch_test_helper import string_type
+from experimental_experiment.torch_models.llm_model_helper import get_phi_35_mini_instruct
+# from experimental_experiment.bench_run import max_diff
+
+
+class SubNeuron(torch.nn.Module):
+    def __init__(self, n_dims: int = 5, n_targets: int = 3):
+        super().__init__()
+        self.linear = torch.nn.Linear(n_dims, n_targets)
+
+    def forward(self, x):
+        z = self.linear(x)
+        return torch.sigmoid(z)
+
+
+class Neuron(torch.nn.Module):
+    def __init__(self, n_dims: int = 5, n_targets: int = 3):
+        super().__init__()
+        self.neuron = SubNeuron(n_dims, n_targets)
+
+    def forward(self, x):
+        z = self.neuron(x)
+        return torch.relu(z)
+
+
+model = Neuron()
+inputs = (torch.randn(1, 5),)
+expected = model(*inputs)
+exported_program = torch.export.export(model, inputs)
+print("-- fx graph with torch.export.export")
+print(exported_program.graph)
+
+#######################################
+# There is no trace left of the sub modules.
+#
+# And through compile
+# ===================
+
+
+def my_compiler(gm, example_inputs):
+    print("-- graph with torch.compile")
+    print(gm.graph)
+    return gm.forward
+
+
+optimized_mod = torch.compile(model, fullgraph=True, backend=my_compiler)
+optimized_mod(*inputs)
+
+################################################
+# Applied to Phi
+# ==============
+
+phi, inputs = get_phi_35_mini_instruct(num_hidden_layers=1, _attn_implementation="eager")
+
+print("model type", type(phi))
+print("inputs:", string_type(inputs))
+
+expected = phi(**inputs)
+
+###################################
+# The module it contains
+# ++++++++++++++++++++++
+
+types = set(type(inst) for _, inst in phi.named_modules())
+print("interesting types:")
+pprint.pprint(list(types))
+
+###################################
+# The exported program.
+# +++++++++++++++++++++
+
+from transformers.models.phi3.modeling_phi3 import logger
+
+logger.warning_once = lambda *_, **__: None
+torch.export._EXPORT_FLAGS = {"non_script"}
+optimized_mod = torch.compile(phi, fullgraph=True, backend=my_compiler)
+optimized_mod(**inputs)
