0be02a2cfdcf06afdd3b3c5d3e61f179d813422d
xadupre <xadupre@microsoft.com>
2024-10-27 19:01:16 +0100

    one more test

--- _unittests/ut_torch_interpreter/test_onnx_export_submodules.py
+++ _unittests/ut_torch_interpreter/test_onnx_export_submodules.py
@@ -8,7 +8,7 @@ from experimental_experiment.reference import ExtendedReferenceEvaluator
 class TestOnnxExportSubModules(ExtTestCase):
 
     @skipif_ci_windows("not available on windows")
-    def test_submodule_local_functions(self):
+    def test_submodule_local_functions_simple(self):
         import torch
 
         class SubNeuron2(torch.nn.Module):
@@ -19,6 +19,48 @@ class TestOnnxExportSubModules(ExtTestCase):
             def forward(self, x):
                 z = self.linear(x)
                 return torch.sigmoid(z)
+
+        class Neuron2(torch.nn.Module):
+            def __init__(self, n_dims: int = 5, n_targets: int = 3):
+                super().__init__()
+                self.neuron = SubNeuron2(n_dims, n_targets)
+
+            def forward(self, x):
+                z = self.neuron(x)
+                return torch.relu(z)
+
+        model = Neuron2()
+        inputs = (torch.randn(1, 5),)
+        expected = model(*inputs)
+        feeds = {"x": inputs[0].numpy()}
+
+        onx = to_onnx(
+            model,
+            inputs,
+            export_modules_as_functions=True,
+            optimize=False,
+            verbose=0,
+        )
+        check_model(onx)
+        self.assertEqual(len(onx.functions), 2)
+        ref = ExtendedReferenceEvaluator(onx)
+        got = ref.run(None, feeds)
+        self.assertEqualArray(expected, got[0], atol=1e-5)
+
+    @skipif_ci_windows("not available on windows")
+    def test_submodule_local_functions_double(self):
+        import torch
+
+        class SubNeuron2(torch.nn.Module):
+            def __init__(self, n_dims: int = 5, n_targets: int = 3):
+                super().__init__()
+                self.linear1 = torch.nn.Linear(n_dims, n_targets)
+                self.linear2 = torch.nn.Linear(n_dims, n_targets)
+
+            def forward(self, x):
+                z1 = self.linear1(x)
+                z2 = self.linear2(x)
+                return torch.sigmoid(z1) + torch.sigmoid(z2)
 
         class Neuron2(torch.nn.Module):
             def __init__(self, n_dims: int = 5, n_targets: int = 3):
