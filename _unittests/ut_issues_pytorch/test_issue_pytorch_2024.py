e3de9e852dd459bc115b123f1c56a0c9ff59c63c
xadupre <xadupre@microsoft.com>
2025-01-23 14:36:10 +0100

    disable failing tests

--- _unittests/ut_issues_pytorch/test_issue_pytorch_2024.py
+++ _unittests/ut_issues_pytorch/test_issue_pytorch_2024.py
@@ -428,6 +428,7 @@ class TestIssuesPytorch2024(ExtTestCase):
     def test_scaled_dot_product_attention_script(self):
         self._scaled_dot_product_attention("script")
 
+    @requires_onnxscript("0.2")
     @hide_stdout()
     def test_scaled_dot_product_attention_dynamo(self):
         self._scaled_dot_product_attention("dynamo")
@@ -493,6 +494,7 @@ class TestIssuesPytorch2024(ExtTestCase):
         expected_output = model(example_input)
         self.assertEqualArray(expected_output, output[0], atol=1e-3)
 
+    @requires_onnxscript("0.2")
     def test_in_projection_packed_script(self):
         self._in_projection_packed("script")
 
@@ -722,6 +724,7 @@ class TestIssuesPytorch2024(ExtTestCase):
     def test_dyn_slice_4d_script(self):
         self._slice_4d("script")
 
+    @requires_onnxscript("0.2")
     @hide_stdout()
     def test_dyn_slice_4d_dynamo(self):
         self._slice_4d("dynamo")
