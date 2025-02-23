0cc8bf3a07850574fb737035f1c1291456174d28
xadupre <xadupre@microsoft.com>
2025-02-23 15:42:43 +0100

    fix example

--- _doc/examples/plot_torch_sklearn_201.py
+++ _doc/examples/plot_torch_sklearn_201.py
@@ -27,18 +27,15 @@ import logging
 import math
 import numbers
 import warnings
-from typing import Any, Dict, List, Optional
 import numpy as np
 import onnx
-from onnx.reference.ops.op_topk import TopK_11 as TopK
 import sklearn
 import torch
 import onnxruntime
 from experimental_experiment.reference import ExtendedReferenceEvaluator
-from experimental_experiment.xbuilder import GraphBuilder
 from experimental_experiment.helpers import max_diff, pretty_onnx
 from experimental_experiment.skl.helpers import flatnonzero, _get_weights
-from experimental_experiment.torch_interpreter import make_undefined_dimension, Dispatcher
+from experimental_experiment.torch_interpreter import make_undefined_dimension
 from experimental_experiment.torch_interpreter.onnx_export_errors import (
     bypass_export_some_errors,
 )
@@ -731,58 +728,11 @@ with warnings.catch_warnings():
         t.exporter_status.exported = t.exporter_status.exported.run_decompositions({})
 
 # %%
-# Let's export everything. Every submodule is exported as a local function
-# except topk for which we must provide an ONNX conversion.
-T = str
-
-
-def onnx_topk_indices(
-    g: GraphBuilder,
-    sts: Optional[Dict[str, Any]],
-    outputs: List[str],
-    x: T,
-    k: T,
-    name: str = "topk",
-):
-    assert len(outputs) == 1, f"Only one output is expected but outputs={outputs}"
-    unique_name = g.unique_name("unused_topk_values")
-    g.op.TopK(x, k, name=name, outputs=[unique_name, *outputs], largest=False, sorted=True)
-    return outputs[0]
-
-
-# %%
-# Let's check it is working somehow.
-
-x = torch.tensor([[0, 1, 2], [6, 5, 4]], dtype=torch.float32)
-print("torch.topk", torch.topk(x, k=2).indices)
-print("onnx.topk", TopK.eval(x.numpy(), np.array([2], dtype=np.int64))[1])
-
-# %%
-# And with nan values
-x = torch.tensor([[0, np.nan, 2], [6, np.nan, 4]], dtype=torch.float32)
-print("torch.topk", torch.topk(torch.nan_to_num(x, nan=-1.0e10), k=2).indices)
-print("onnx.topk", TopK.eval(x.numpy(), np.array([2], dtype=np.int64))[1])
-
-
-# %%
-# That works. Then the dispatcher maps the custom ops calling topk to
-# the previous converter functions.
-
-prefix = "diag_lib::C_TorchKNNImputer_columns"
-dispatcher = Dispatcher(
-    {
-        f"{prefix}_{i}___calc_impute__donors_idx__topk": onnx_topk_indices
-        for i in range(X.shape[1])
-    }
-)
-
-# %%
 # Let's run the conversion. We also check the conversion into ONNX
 # is accurate. It is doable because every intermediate results
 # were previously traced.
 onx = trace.to_onnx_local(
     verbose=1,
-    dispatcher=dispatcher,
     check_conversion_cls=dict(cls=ExtendedReferenceEvaluator, atol=1e-5, rtol=1e-5),
     inline=False,
 )
