29fcd2b9fc6580fdda57fd480e9b26c27583a17d
xadupre <xadupre@microsoft.com>
2025-07-03 20:38:12 +0200

    fix import issues

--- experimental_experiment/reference/__init__.py
+++ experimental_experiment/reference/__init__.py
@@ -1,4 +1,7 @@
-from onnx.reference.op_run import to_array_extended  # noqa: F401
+try:
+    from onnx.reference.op_run import to_array_extended  # noqa: F401
+except ImportError:
+    from onnx.numpy_helper import to_array as to_array_extended  # noqa: F401
 from ..helpers import from_array_extended  # noqa: F401
 from .evaluator import ExtendedReferenceEvaluator  # noqa: F401
 from .ort_evaluator import OrtEval  # noqa: F401
