ed9c9b3f165a3caba7f8526544ffa06d1233d36b
xadupre <xadupre@microsoft.com>
2025-10-16 17:39:31 +0200

    add another function

--- experimental_experiment/torch_interpreter/onnx_export.py
+++ experimental_experiment/torch_interpreter/onnx_export.py
@@ -458,6 +458,7 @@ def get_default_aten_as_function(target_opset: Optional[int] = None) -> Tuple[st
     return (
         "aten.index_copy.default",
         "aten.index_put.default",
+        "aten.index.Tensor",
         # "aten.scaled_dot_product_attention.default",
         "aten.setitem",
         operator.setitem,
