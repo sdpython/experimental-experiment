d07198d2bef57b5774196e97d489be1f793f3ba8
Xavier Dupré <xadupre@microsoft.com>
2025-11-12 15:45:08 +0000

    add missing kernel

---
+++ experimental_experiment/xoptim/patterns_ort/missing_kernels.py
@@ -0,0 +1,62 @@
+import inspect
+from typing import List, Optional
+from onnx import NodeProto, TensorProto
+from ..patterns_api import MatchResult, PatternOptimization
+
+
+class MissingRangePattern(PatternOptimization):
+    """Replaces Range by Cast Range Cast because of some missing kernels."""
+
+    def match(
+        self,
+        g: "GraphBuilderPatternOptimization",  # noqa: F821
+        node: NodeProto,
+        matched: List[MatchResult],
+    ) -> Optional[MatchResult]:
+        if node.op_type != "Range" or node.domain != "":
+            return self.none()
+        if not g.has_type(node.input[0]) or g.get_type(node.input[0]) in {
+            TensorProto.INT32,
+            TensorProto.INT64,
+            TensorProto.FLOAT32,
+        }:
+            return self.none(node, inspect.currentframe().f_lineno)
+        return MatchResult(self, [node], self.apply, insert_at=node)
+
+    def apply(
+        self,
+        g: "GraphBuilder",  # noqa: F821
+        node: NodeProto,
+    ) -> List[NodeProto]:
+        to = g.get_type(node.input[0])
+        other_to = (
+            TensorProto.FLOAT
+            if to in {TensorProto.FLOAT16, TensorProto.BFLOAT16}
+            else TensorProto.INT32
+        )
+        nodes = []
+        new_inputs = []
+        for i in node.input:
+            n = g.unique_name(f"{self.__class__.__name__}--{i}")
+            nodes.append(
+                g.make_node(
+                    "Cast", [i], [n], to=other_to, name=f"{self.__class__.__name__}--Cast"
+                )
+            )
+            new_inputs.append(n)
+        n = g.unique_name(f"{self.__class__.__name__}--{i}")
+        nodes.append(
+            g.make_node(
+                node.op_type,
+                new_inputs,
+                [n],
+                domain=node.domain,
+                name=f"{self.__class__.__name__}",
+            )
+        )
+        nodes.append(
+            g.make_node(
+                "Cast", [n], [node.output[0]], to=to, name=f"{self.__class__.__name__}--Cast"
+            )
+        )
+        return nodes
