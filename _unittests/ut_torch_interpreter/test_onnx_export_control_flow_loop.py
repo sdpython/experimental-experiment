cafe4feb6c4e09bac4c8b87b0b50059b0a1316c0
Xavier Dupré <xadupre@microsoft.com>
2025-12-13 00:15:08 +0100

    fix

--- _unittests/ut_torch_interpreter/test_onnx_export_control_flow_loop.py
+++ _unittests/ut_torch_interpreter/test_onnx_export_control_flow_loop.py
@@ -1,9 +1,15 @@
 import unittest
-from experimental_experiment.ext_test_case import ExtTestCase, requires_onnx_diagnostic
+from experimental_experiment.ext_test_case import (
+    ExtTestCase,
+    requires_onnx_diagnostic,
+    requires_torch,
+)
 from experimental_experiment.torch_interpreter import to_onnx
+from onnx_diagnostic.export.control_flow_onnx import loop_for_onnx
 
 
 class TestOnnxExportControlLoop(ExtTestCase):
+    @requires_torch("2.9.99")
     @requires_onnx_diagnostic("0.8.6")
     def test_simple_loop_for_1(self):
         import torch
@@ -26,6 +32,44 @@ class TestOnnxExportControlLoop(ExtTestCase):
         feeds = dict(n_iter=n_iter, x=x.numpy())
         got = ref.run(None, feeds)[0]
         self.assertEqualArray(expected, got)
+
+    @requires_torch("2.9.99")
+    @requires_onnx_diagnostic("0.8.6")
+    def test_loop_two_custom_concatenation_dims(self):
+        import torch
+
+        class Model(torch.nn.Module):
+            def forward(self, n_iter, x):
+                def body(i, x):
+                    return x[: i.item() + 1].unsqueeze(1), x[: i.item() + 1].unsqueeze(0) + 1
+
+                res = loop_for_onnx(n_iter, body, (x,), concatenation_dims=[0, 1])
+                return res[0] + res[1].T
+
+        model = Model()
+        n_iter = torch.tensor(4, dtype=torch.int64)
+        x = torch.arange(10, dtype=torch.float32)
+        expected = torch.tensor([1, 1, 3, 1, 3, 5, 1, 3, 5, 7], dtype=x.dtype).unsqueeze(1)
+        got = model(n_iter, x)
+        self.assertEqualArray(expected, got)
+
+        ep = torch.export.export(
+            model, (n_iter, x), dynamic_shapes=({}, ({0: torch.export.Dim.DYNAMIC}))
+        )
+        self.assertIn(
+            "torch.ops.onnx_higher_ops.loop_for_onnx_TestControlFlowOnnx_test_loop_two_custom_concatenation_dims_L_Model_forward_L_body_",
+            str(ep),
+        )
+
+        onx = to_onnx(
+            model,
+            (n_iter, x),
+            dynamic_shapes=({}, ({0: torch.export.Dim.DYNAMIC})),
+            exporter="custom",
+            use_control_flow_dispatcher=True,
+        ).model_proto
+        self.dump_onnx("test_loop_one_custom.onnx", onx)
+        self.assert_onnx_disc("test_loop_one_custom", onx, model, (n_iter, x))
 
 
 if __name__ == "__main__":
--- experimental_experiment/__init__.py
+++ experimental_experiment/__init__.py
@@ -2,7 +2,7 @@
 Fuzzy work.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Xavier Dupré"
 
 
