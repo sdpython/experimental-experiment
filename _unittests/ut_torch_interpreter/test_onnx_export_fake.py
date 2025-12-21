7dd49aa4457a6d44f97cd47e5823e5a8b717ae43
Xavier Dupré <xadupre@microsoft.com>
2025-12-21 18:36:46 +0100

    fix one issue with fake dimension

--- _unittests/ut_torch_interpreter/test_onnx_export_fake.py
+++ _unittests/ut_torch_interpreter/test_onnx_export_fake.py
@@ -75,6 +75,31 @@ class TestOnnxExportInputList(ExtTestCase):
         got = ref.run(None, {"x": x.numpy()})
         self.assertEqualArray(expected, got[0], atol=1e-5)
 
+    def test_export_with_option_fake_2(self):
+        import torch
+
+        class Neuron(torch.nn.Module):
+            def __init__(self, n_dims: int, n_targets: int):
+                super(Neuron, self).__init__()
+                self.linear = torch.nn.Linear(n_dims, n_targets)
+
+            def forward(self, x, y):
+                return torch.relu(self.linear(x)) + torch.relu(self.linear(y))
+
+        model = Neuron(5, 3)
+        x, y = torch.rand(2, 5), torch.rand(2, 5)
+        expected = model(x, y)
+        onx = to_onnx(
+            model,
+            (x, y),
+            dynamic_shapes=({0: "batch"}, {0: "batch"}),
+            export_options=ExportOptions(fake=True),
+            verbose=0,
+        )
+        ref = ExtendedReferenceEvaluator(onx)
+        got = ref.run(None, {"x": x.numpy(), "y": y.numpy()})
+        self.assertEqualArray(expected, got[0], atol=1e-5)
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
