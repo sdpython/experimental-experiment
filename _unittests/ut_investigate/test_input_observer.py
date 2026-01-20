9ab8d8a9fa6a818b65367530a9a61bf3465418ae
Xavier Dupré <xadupre@microsoft.com>
2026-01-20 15:34:15 +0100

    regular

--- _unittests/ut_investigate/test_input_observer.py
+++ _unittests/ut_investigate/test_input_observer.py
@@ -1,11 +1,18 @@
 import unittest
 import torch
 from experimental_experiment.ext_test_case import ExtTestCase
-from experimental_experiment.investigate.input_observer import InputObserver
+from experimental_experiment.investigate.input_observer import (
+    InputObserver,
+    infer_dynamic_dimensions,
+)
 
 
 class TestTracingVeector(ExtTestCase):
-    def test_io_captured(self):
+    def test_infer_dynamic_dimensions(self):
+        self.assertEqual([2], infer_dynamic_dimensions([(1, 2, 3), (1, 2, 4)]))
+        self.assertEqual([0, 2], infer_dynamic_dimensions([(1, 2, 3), (2, 2, 4)]))
+
+    def test_io_captured_args(self):
         class Model(torch.nn.Module):
             def forward(self, x, y):
                 return x + y
@@ -27,6 +34,213 @@ class TestTracingVeector(ExtTestCase):
         for i in range(3):
             self.assertEqual(len(observer.info.flat_outputs[i]), 1)
             torch.testing.assert_close(expected[i], observer.info.flat_outputs[i][0])
+
+        cst = torch.export.Dim.DYNAMIC
+        self.assertEqual(({0: cst, 1: cst}, {1: cst}), observer.infer_dynamic_shapes())
+
+    def test_io_captured_kwargs(self):
+        class Model(torch.nn.Module):
+            def forward(self, x, y):
+                return x + y
+
+        inputs = [
+            dict(x=torch.randn((5, 6)), y=torch.randn((1, 6))),
+            dict(x=torch.randn((7, 7)), y=torch.randn((1, 7))),
+            dict(x=torch.randn((7, 8)), y=torch.randn((1, 8))),
+            dict(x=torch.randn((7, 9)), y=torch.randn((1, 9))),
+        ]
+
+        model = Model()
+        expected = [model(**kwargs) for kwargs in inputs]
+        observer = InputObserver()
+        with observer(model):
+            for kwargs in inputs:
+                model(**kwargs)
+        self.assertEqual(len(observer.info), 3)
+        for i in range(3):
+            self.assertEqual(len(observer.info.flat_outputs[i]), 1)
+            torch.testing.assert_close(expected[i], observer.info.flat_outputs[i][0])
+
+        cst = torch.export.Dim.DYNAMIC
+        self.assertEqual(dict(x={0: cst, 1: cst}, y={1: cst}), observer.infer_dynamic_shapes())
+
+    def test_io_captured_kwargs_bool(self):
+        class Model(torch.nn.Module):
+            def forward(self, x, y, add=True):
+                if add:
+                    return x + y
+                return x - y
+
+        inputs = [
+            dict(x=torch.randn((5, 6)), y=torch.randn((1, 6)), add=False),
+            dict(x=torch.randn((7, 7)), y=torch.randn((1, 7)), add=False),
+            dict(x=torch.randn((7, 8)), y=torch.randn((1, 8)), add=False),
+            dict(x=torch.randn((7, 9)), y=torch.randn((1, 9)), add=False),
+        ]
+
+        model = Model()
+        expected = [model(**kwargs) for kwargs in inputs]
+        observer = InputObserver()
+        with observer(model):
+            for kwargs in inputs:
+                model(**kwargs)
+        self.assertEqual(len(observer.info), 3)
+        for i in range(3):
+            self.assertEqual(len(observer.info.flat_outputs[i]), 1)
+            torch.testing.assert_close(expected[i], observer.info.flat_outputs[i][0])
+
+        cst = torch.export.Dim.DYNAMIC
+        self.assertEqual(dict(x={0: cst, 1: cst}, y={1: cst}), observer.infer_dynamic_shapes())
+
+    def test_io_captured_args_kwargs(self):
+        class Model(torch.nn.Module):
+            def forward(self, x, y, z=None, w=None):
+                r = x + y
+                if z is not None:
+                    r += z
+                if w is not None:
+                    r += w
+                return r
+
+        inputs = [
+            (
+                (torch.randn((5, 6)), torch.randn((1, 6))),
+                dict(z=torch.randn((5, 6)), w=torch.randn((1, 6))),
+            ),
+            (
+                (torch.randn((6, 7)), torch.randn((1, 7))),
+                dict(z=torch.randn((6, 7)), w=torch.randn((1, 7))),
+            ),
+            (
+                (torch.randn((7, 8)), torch.randn((1, 8))),
+                dict(z=torch.randn((7, 8)), w=torch.randn((1, 8))),
+            ),
+            (
+                (torch.randn((8, 9)), torch.randn((1, 9))),
+                dict(z=torch.randn((8, 9)), w=torch.randn((1, 9))),
+            ),
+        ]
+
+        model = Model()
+        expected = [model(*args, **kwargs) for args, kwargs in inputs]
+        observer = InputObserver()
+        with observer(model):
+            for args, kwargs in inputs:
+                model(*args, **kwargs)
+        self.assertEqual(len(observer.info), 3)
+        for i in range(3):
+            self.assertEqual(len(observer.info.flat_outputs[i]), 1)
+            torch.testing.assert_close(expected[i], observer.info.flat_outputs[i][0])
+
+        cst = torch.export.Dim.DYNAMIC
+        self.assertEqual(
+            dict(x={0: cst, 1: cst}, y={1: cst}, z={0: cst, 1: cst}, w={1: cst}),
+            observer.infer_dynamic_shapes(),
+        )
+
+    def test_io_captured_optional_args(self):
+        class Model(torch.nn.Module):
+            def forward(self, x, y=None):
+                if y is None:
+                    return x
+                return x - y
+
+        inputs = [
+            (torch.randn((5, 6)),),
+            (torch.randn((6, 7)), torch.randn((1, 7))),
+            (torch.randn((7, 8)), torch.randn((1, 8))),
+            (torch.randn((8, 9)), torch.randn((1, 9))),
+        ]
+
+        model = Model()
+        expected = [model(*args) for args in inputs]
+        observer = InputObserver()
+        with observer(model):
+            for args in inputs:
+                model(*args)
+        self.assertEqual(len(observer.info), 3)
+        for i in range(3):
+            self.assertEqual(len(observer.info.flat_outputs[i]), 1)
+            torch.testing.assert_close(expected[i], observer.info.flat_outputs[i][0])
+
+        cst = torch.export.Dim.DYNAMIC
+        self.assertEqual(({0: cst, 1: cst}, {1: cst}), observer.infer_dynamic_shapes())
+
+    def test_io_captured_optional_kwargs(self):
+        class Model(torch.nn.Module):
+            def forward(self, x, y=None):
+                if y is None:
+                    return x
+                return x - y
+
+        inputs = [
+            dict(
+                x=torch.randn((5, 6)),
+            ),
+            dict(x=torch.randn((6, 7)), y=torch.randn((1, 7))),
+            dict(x=torch.randn((7, 8)), y=torch.randn((1, 8))),
+            dict(x=torch.randn((8, 9)), y=torch.randn((1, 9))),
+        ]
+
+        model = Model()
+        expected = [model(**kwargs) for kwargs in inputs]
+        observer = InputObserver()
+        with observer(model):
+            for kwargs in inputs:
+                model(**kwargs)
+        self.assertEqual(len(observer.info), 3)
+        for i in range(3):
+            self.assertEqual(len(observer.info.flat_outputs[i]), 1)
+            torch.testing.assert_close(expected[i], observer.info.flat_outputs[i][0])
+
+        cst = torch.export.Dim.DYNAMIC
+        self.assertEqual(dict(x={0: cst, 1: cst}, y={1: cst}), observer.infer_dynamic_shapes())
+
+    def test_io_captured_optional_args_kwargs(self):
+        class Model(torch.nn.Module):
+            def forward(self, x, y=None, z=None, w=None):
+                r = x + y if y is not None else x
+                if z is not None:
+                    r += z
+                if w is not None:
+                    r += w
+                return r
+
+        inputs = [
+            (
+                (torch.randn((5, 6)),),
+                dict(w=torch.randn((1, 6))),
+            ),
+            (
+                (torch.randn((6, 7)), torch.randn((1, 7))),
+                dict(z=torch.randn((6, 7)), w=torch.randn((1, 7))),
+            ),
+            (
+                (torch.randn((7, 8)), torch.randn((1, 8))),
+                dict(z=torch.randn((7, 8)), w=torch.randn((1, 8))),
+            ),
+            (
+                (torch.randn((8, 9)), torch.randn((1, 9))),
+                dict(z=torch.randn((8, 9)), w=torch.randn((1, 9))),
+            ),
+        ]
+
+        model = Model()
+        expected = [model(*args, **kwargs) for args, kwargs in inputs]
+        observer = InputObserver()
+        with observer(model):
+            for args, kwargs in inputs:
+                model(*args, **kwargs)
+        self.assertEqual(len(observer.info), 3)
+        for i in range(3):
+            self.assertEqual(len(observer.info.flat_outputs[i]), 1)
+            torch.testing.assert_close(expected[i], observer.info.flat_outputs[i][0])
+
+        cst = torch.export.Dim.DYNAMIC
+        self.assertEqual(
+            dict(x={0: cst, 1: cst}, y={1: cst}, z={0: cst, 1: cst}, w={1: cst}),
+            observer.infer_dynamic_shapes(),
+        )
 
 
 if __name__ == "__main__":
--- experimental_experiment/investigate/input_observer.py
+++ experimental_experiment/investigate/input_observer.py
@@ -3,25 +3,151 @@ import inspect
 import torch
 
 
+def infer_dynamic_dimensions(shape_list: tuple[int, ...]) -> list[int]:
+    """
+    Returns the list dynamic dimensions given a list of shapes corresponding to the same tensor.
+    """
+    unique_ranks = {len(shape) for shape in shape_list}
+    torch._check(
+        len(unique_ranks) == 1, lambda: "all shapes in shape_list must have the the same rank"
+    )
+    rank = unique_ranks.pop()
+    dynamic = []
+    for i in range(rank):
+        dims = [shape[i] for shape in shape_list]
+        if len(set(dims)) > 1:
+            dynamic.append(i)
+    return dynamic
+
+
 class InputObserverInfo:
-    def __init__(self):
+    def __init__(self, signature):
         self.inputs_specs = []
         self.flat_inputs = []
+        self.n_args = []
+        self.kwargs_keys = []
         self.outputs_specs = []
         self.flat_outputs = []
+        self.signature = signature
+
+        self._max_args = None
+        self._max_kwargs = None
+        self._spec = None
 
     def __len__(self) -> int:
         return len(self.flat_inputs)
 
     def add_inputs(self, args, kwargs):
+        kwargs = {
+            k: v
+            for k, v in kwargs.items()
+            if v is not None and not isinstance(v, (int, float, bool))
+        }
         flat_args, spec = torch.utils._pytree.tree_flatten((args, kwargs))
         self.inputs_specs.append(spec)
-        self.flat_inputs.append([t.clone().detach() for t in flat_args])
+        self.n_args.append(len(args))
+        self.kwargs_keys.append(tuple(kwargs))
+        cloned = [t.clone().detach() for t in flat_args]
+        self.flat_inputs.append(cloned)
+
+        cloned_args, cloned_kwargs = torch.utils._pytree.tree_unflatten(cloned, spec)
+        if self._max_args is None or len(cloned_args) > len(self._max_args):
+            self._max_args = cloned_args
+        if self._max_kwargs is None or len(cloned_kwargs) > len(self._max_kwargs):
+            self._max_kwargs = cloned_kwargs
 
     def add_outputs(self, res):
         flat_res, spec = torch.utils._pytree.tree_flatten(res)
         self.outputs_specs.append(spec)
         self.flat_outputs.append([t.clone().detach() for t in flat_res])
+
+    def build_inputs_completed_with_none_values(self):
+        sizes = [len(flat) for flat in self.flat_inputs]
+        if len(set(sizes)) == 1:
+            return self.flat_inputs, self.inputs_specs[0]
+        # The flattened list has not always the same size. We need to align
+        # all the sequences. A necessary condition to detect that cases is
+        # len(set(sizes)) != 1, but even if this condition is verified,
+        # we could find a case where by chance the number of flattened tensors
+        # is always the same even though it is coming from different inputs.
+        # This case is not detected but still handled by that function
+        # assuming this does not happen in one of the nested structures.
+
+        # Let's compute the sizes of each indenpendently.
+        max_spec = torch.utils._pytree.tree_flatten((self._max_args, self._max_kwargs))[1]
+        arg_sizes = [len(torch.utils._pytree.tree_flatten(a)[0]) for a in self._max_args]
+        kwarg_sizes = {
+            k: len(torch.utils._pytree.tree_flatten(v)[0]) for k, v in self._max_kwargs.items()
+        }
+
+        # Let's reprocess everything.
+        new_flat_inputs = []
+        for args_kwargs, spec in zip(self.flat_inputs, self.inputs_specs):
+            args, kwargs = torch.utils._pytree.tree_unflatten(args_kwargs, spec)
+            if len(set(kwargs) | set(self._max_kwargs)) > len(self._max_kwargs):
+                raise RuntimeError(
+                    "At least one call to the observed model "
+                    "must contain all the named arguments."
+                )
+            flat = []
+            for i in range(len(self._max_args)):
+                if i < len(args):
+                    flat.extend(torch.utils._pytree.tree_flatten(args[i])[0])
+                else:
+                    flat.extend([None for _ in range(arg_sizes[i])])
+            for k in self._max_kwargs:
+                if k in kwargs:
+                    flat.extend(torch.utils._pytree.tree_flatten(kwargs[k])[0])
+                else:
+                    flat.extend([None for _ in range(kwarg_sizes[k])])
+            new_flat_inputs.append(flat)
+        return new_flat_inputs, max_spec
+
+    def infer_dynamic_shapes(self):
+        if not self.flat_inputs:
+            raise RuntimeError("No inputs were captured.")
+        flat_inputs, max_spec = self.build_inputs_completed_with_none_values()
+        if len({len(flat) for flat in flat_inputs}) != 1:
+            raise NotImplementedError(
+                "infer_dynamic_shapes is not implemented "
+                "when the number of input tensors are not the same."
+            )
+        shape_lists = [
+            [(None if t is None else t.shape) for t in tensors] for tensors in flat_inputs
+        ]
+        n_tensors = len(shape_lists[0])
+        dynamic_shapes = [
+            infer_dynamic_dimensions(
+                [s for s in [shapes[index] for shapes in shape_lists] if s is not None]
+            )
+            for index in range(n_tensors)
+        ]
+        cst = torch.export.Dim.DYNAMIC
+        flat_dynamic_shapes = [dict.fromkeys(dims, cst) for dims in dynamic_shapes]
+        if len(flat_dynamic_shapes) == len(self._max_args) + len(self._max_kwargs):
+            # It means forward method is called with tensors only.
+            if not self._max_kwargs:
+                # only positional arguments
+                return tuple(flat_dynamic_shapes)
+            if not self._max_args:
+                # only named arguments
+                return dict(zip(list(self._max_kwargs), flat_dynamic_shapes))
+            # positional arguments needs to be moved to the named arguments
+            n_args = len(self._max_args)
+            pos_names = list(self.signature.parameters)[:n_args]
+            return {
+                **dict(zip(pos_names, flat_dynamic_shapes[:n_args])),
+                **dict(zip(list(self._max_kwargs), flat_dynamic_shapes[n_args:])),
+            }
+        # nested types, here comes the fun part because the the shapes cannot be unflattened,
+        # custom classes must appear in their flattened shape.
+        raise NotImplementedError(
+            f"There are nested types in the inputs, "
+            f"len(flat_dynamic_shapes)={len(flat_dynamic_shapes)}, "
+            f"len(self._max_args)={len(self._max_args)}, "
+            f"len(self._max_kwargs)={len(self._max_kwargs)}, "
+            f"max_spec={max_spec}"
+        )
 
 
 class InputObserver:
@@ -45,8 +171,7 @@ class InputObserver:
             raise RuntimeError(
                 "This class was already used to capture a model. Please create a new one."
             )
-        self.info = InputObserverInfo()
-        self.info.signature = inspect.signature(model.forward)
+        self.info = InputObserverInfo(signature=inspect.signature(model.forward))
         forward_method = model.forward
         model.forward = (
             lambda *args, _captured_forward=forward_method, **kwargs: self._forward_captured(
@@ -57,3 +182,6 @@ class InputObserver:
             yield self
         finally:
             model.forward = forward_method
+
+    def infer_dynamic_shapes(self):
+        return self.info.infer_dynamic_shapes()
